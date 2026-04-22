e0c0a740a00611f91800c81a28c873b26bff23a2
gomex500 <gomezfreddy886@gmail.com>
2026-04-22 01:07:18 -0600

    cuato guardado

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 from config.config import DEBUG, PORT
 from flask_cors import CORS
-from routers.login_router import login_routes
-from routers.user_router import user_routes
+# from routers.login_router import login_routes
+# from routers.user_router import user_routes
 from routers.home_router import home
 from routers.web_auth import web_auth
 
@@ -13,10 +13,10 @@ CORS(app)
 
 
 #routes
-# app.register_blueprint(home)
-app.register_blueprint(login_routes)
-app.register_blueprint(user_routes)
-app.register_blueprint(web_auth)
+app.register_blueprint(home)
+# app.register_blueprint(login_routes)
+# app.register_blueprint(user_routes)
+# app.register_blueprint(web_auth)
 
 
 
--- routers/home_router.py
+++ routers/home_router.py
@@ -1,16 +1,26 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 from controllers.jwt import validar_token
 
 #inicializando ruta
 home = Blueprint('home', __name__)
 
-#ruta inicial
-@home.route('/')
-def index():
-    return '<h1>welcome to api SCI</h1>'
+# #ruta inicial
+# @home.route('/')
+# def index():
+#     return '<h1>welcome to api SCI</h1>'
 
 #ruta verificadora de token
 @home.route('/token')
 def verificar():
     token = request.headers['Authorization'].split(" ")[1]
     return validar_token(token, output=True)
+
+#ruta inicial
+@home.route('/')
+def index():
+    return render_template('login.html')
+
+#ruta inicial
+@home.route('/register')
+def register():
+    return render_template('register.html')
